4684e12dd19271170660c4d8ed5633852ef0e3c1
NasrEddineDev <mehamednasreddine@gmail.com>
2020-10-22 09:29:01 +0100

    last commit

---
+++ documents/PFE 3D-Secure E-Commerce.mm
@@ -0,0 +1,128 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="PFE 3D-Secure E-Paiement" FOLDED="false" ID="ID_6989720" CREATED="1576678195796" MODIFIED="1576686883884" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="M&#xe9;moire" POSITION="right" ID="ID_903585358" CREATED="1576678241329" MODIFIED="1576678247695">
+<edge COLOR="#ff0000"/>
+<node TEXT="Etat de l&apos;art" ID="ID_692861128" CREATED="1576678287815" MODIFIED="1576678304928">
+<node TEXT="Ch01: E-paiement" ID="ID_1785098958" CREATED="1576678392274" MODIFIED="1576678474623">
+<node TEXT="01 Introduction" ID="ID_1179418523" CREATED="1576869736382" MODIFIED="1576869779947"/>
+<node TEXT="02 Definition" ID="ID_924068322" CREATED="1576869784763" MODIFIED="1576869800433"/>
+<node TEXT="03 Historique" ID="ID_1710227507" CREATED="1576869805541" MODIFIED="1576869827147"/>
+<node TEXT="04 Architecture" ID="ID_41124677" CREATED="1576869841515" MODIFIED="1576869898952"/>
+<node TEXT="05 Classification et Types" ID="ID_454881376" CREATED="1576869903660" MODIFIED="1576869930998"/>
+<node TEXT="06 Caracteristiques" ID="ID_1474521794" CREATED="1576869934512" MODIFIED="1576869987211"/>
+<node TEXT="07 Conclusion" ID="ID_1512309871" CREATED="1576869992610" MODIFIED="1576870013032"/>
+</node>
+<node TEXT="Ch02: Securit&#xe9;" ID="ID_1227307490" CREATED="1576678421052" MODIFIED="1577782476441">
+<node TEXT="01 Introduction" ID="ID_451212362" CREATED="1577780607146" MODIFIED="1577780616533"/>
+<node TEXT="02 Les exigences de la s&#xe9;curit&#xe9;" ID="ID_1239514929" CREATED="1577780616971" MODIFIED="1577780724867">
+<node TEXT="02.1 L&apos;authentification" ID="ID_1902922336" CREATED="1577781022448" MODIFIED="1577781100855"/>
+<node TEXT="02.2. La confidentialit&#xe9; des donn&#xe9;es" ID="ID_925104365" CREATED="1577781041822" MODIFIED="1577781095378"/>
+<node TEXT="02.3. L&apos;integrit&#xe9; des donn&#xe9;es" ID="ID_330019260" CREATED="1577781068208" MODIFIED="1577781087621"/>
+<node TEXT="02.4. La fiabilit&#xe9;" ID="ID_296516130" CREATED="1577781129580" MODIFIED="1577781137904"/>
+<node TEXT="02.5. La validit&#xe9; des donn&#xe9;es" ID="ID_818284630" CREATED="1577782242419" MODIFIED="1577782267121"/>
+<node TEXT="02.6. La non-r&#xe9;pudiation des informations" ID="ID_81125741" CREATED="1577782317261" MODIFIED="1577782323296"/>
+</node>
+<node TEXT="Les attaques" ID="ID_1125849846" CREATED="1577784540377" MODIFIED="1577784571459">
+<node TEXT="Worms" ID="ID_1834318410" CREATED="1577784577330" MODIFIED="1577784582283"/>
+<node TEXT="Trojan  horse" ID="ID_1942370531" CREATED="1577784582784" MODIFIED="1577784595240"/>
+<node TEXT="DoS (denial-of-service)" ID="ID_366969130" CREATED="1577784616117" MODIFIED="1577784634944"/>
+<node TEXT="Phishing  and  Pharming" ID="ID_1572057528" CREATED="1577784664400" MODIFIED="1577784667093"/>
+</node>
+<node TEXT="03 Les modeles de payment par carte" ID="ID_615337116" CREATED="1577781842803" MODIFIED="1577786362613">
+<node TEXT="Sans s&#xe9;curit&#xe9;" ID="ID_1615727694" CREATED="1577781911561" MODIFIED="1577782105468"/>
+<node TEXT="En utilisant une partie tiers comme intermidiaire" ID="ID_1942888964" CREATED="1577782056919" MODIFIED="1577782099343"/>
+<node TEXT="Chiffrement simple" ID="ID_657563935" CREATED="1577782107769" MODIFIED="1577782165525"/>
+<node TEXT="SET (security electronic transaction)" ID="ID_553647080" CREATED="1577782169049" MODIFIED="1577782190467"/>
+</node>
+<node TEXT="03 Les protocoles de s&#xe9;curit&#xe9;" ID="ID_20171367" CREATED="1577780698151" MODIFIED="1577780717418">
+<node TEXT="03.1 Security Socket Layer protocol SSL" ID="ID_1206076915" CREATED="1577780752718" MODIFIED="1577780783186"/>
+<node TEXT="03.2 Secure Electronic Transaction (SET)" ID="ID_760031646" CREATED="1577780783704" MODIFIED="1577780798950"/>
+</node>
+<node TEXT="Le Syst&#xe8;me 3D Security" ID="ID_870543637" CREATED="1577782469709" MODIFIED="1577782506530">
+<node TEXT="Architecture" ID="ID_878022309" CREATED="1577787784977" MODIFIED="1577787792613"/>
+<node TEXT="Le processus d&apos;une transaction 3d-secure" ID="ID_337199398" CREATED="1577787795094" MODIFIED="1577787946001"/>
+</node>
+<node TEXT="Conclusion" ID="ID_796570086" CREATED="1577786400933" MODIFIED="1577786407371"/>
+</node>
+</node>
+<node TEXT="Solution" ID="ID_1782350918" CREATED="1576678324795" MODIFIED="1576678346690"/>
+<node TEXT="Demonstration" ID="ID_1990710918" CREATED="1576678354036" MODIFIED="1576678364626"/>
+</node>
+<node TEXT="Realisation" POSITION="left" ID="ID_1865631630" CREATED="1576678251326" MODIFIED="1576678279343">
+<edge COLOR="#0000ff"/>
+<node TEXT="App Web" ID="ID_1619200188" CREATED="1576678538144" MODIFIED="1576678554913"/>
+<node TEXT="App Mobile" ID="ID_779321391" CREATED="1576678559891" MODIFIED="1576678568714"/>
+<node TEXT="Enviroment de travaille" ID="ID_6404354" CREATED="1576686896029" MODIFIED="1576686918893"/>
+</node>
+<node TEXT="Pr&#xe9;sentation" POSITION="right" ID="ID_423546495" CREATED="1576686946560" MODIFIED="1576686956396">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Publication" POSITION="left" ID="ID_1354501434" CREATED="1576686963770" MODIFIED="1576686992576">
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
